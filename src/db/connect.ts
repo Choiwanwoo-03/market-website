afdcc22c5b0b75b4a9b74d70df62d6d639c6cdc1
Choiwanwoo-03 <chldhksdn37@gmail.com>
2026-06-01 17:29:52 +0900

    feat : MONGODB_URI를 함수 내부로 이동

--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,11 +1,5 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI!
-
-if (!MONGODB_URI) {
-  throw new Error('MONGODB_URI 환경변수가 설정되지 않았습니다.')
-}
-
 let cached = global.mongoose
 
 if (!cached) {
@@ -13,6 +7,12 @@ if (!cached) {
 }
 
 async function dbConnect() {
+  const MONGODB_URI = process.env.MONGODB_URI
+
+  if (!MONGODB_URI) {
+    throw new Error('MONGODB_URI 환경변수가 설정되지 않았습니다.')
+  }
+
   if (cached.conn) {
     return cached.conn
   }
